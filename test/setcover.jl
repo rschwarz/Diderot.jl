be3ca38f932dd31b7e0775d5ff04e6c0020709f2
Robert Schwarz <mail@rschwarz.net>
2020-05-12 21:07:14 +0200

    add tests for exact decision diagram (SetCover)

--- test/setcover.jl
+++ test/setcover.jl
@@ -24,3 +24,33 @@ const SC = SetCover
     @test Diderot.transitions(instance, BitSet(1:3), 4) ==
         Dict(Arc(BitSet(1:3), true, -2.0) => BitSet(1:2))
 end
+
+@testset "exact decision diagram" begin
+    N = Node{BitSet,Bool,Float64}
+
+    values = [-2.0, -3.0, -4.0, -2.0]
+    sets = sparse(Bool[1 1 0 0; 0 1 1 0; 0 0 1 1])
+    instance = SC.Instance(values, sets)
+
+    # build diagram
+    diagram = Diderot.Diagram(instance)
+    Diderot.top_down!(diagram, instance)
+
+    @test length(diagram.layers) == 5
+
+    @test length(diagram.layers[1]) == 1
+    @test diagram.layers[1][BitSet(1:3)] == N(0.0, nothing)
+
+    @test length(diagram.layers[2]) == 2
+    @test diagram.layers[2][BitSet(1:3)] ==
+        N(0.0, Arc(BitSet(1:3), false, 0.0))
+    @test diagram.layers[2][BitSet(2:3)] ==
+        N(-2.0, Arc(BitSet(1:3), true, -2.0))
+
+    # and so on ...
+
+    @test length(diagram.layers[5]) == 1  # terminal
+
+    # extract solution
+    @test Diderot.longest_path(diagram) == Diderot.Solution([0, 1, 0, 1], -5.0)
+end
